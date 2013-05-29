6901ddbc2bacdd45fbeec3675dbee2cd37afc5b6
CSIGI <trannguyenbaotrung@gmail.com>
2013-05-29 11:26:13 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhoneAddressBook::Application.config.secret_token = '52d23bfc6d84b70e95ced4eec2826e49b0bcec3d9e56b65cb754f1a902c1dc4008bde2727d607ed774d63f43ebc85a6479505573a91a3f97a6830cbd95c2a0ec'
